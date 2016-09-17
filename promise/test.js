3101c3c2663e6b2a68ae8fc1d8c383b78b87859d
Samuel Xu <sam618.xu@qq.com>
2016-09-17 23:33:26 +0800

    commit

--- promise/test.js
+++ promise/test.js
@@ -2,8 +2,25 @@
 
 var Promise = require('bluebird');
 
-var p = Promise.resolve('Hello');
+var p = Promise.resolve('I\'m done');
 
-p.then(function (s){
-  console.log(s);   //Hello
+p.then(function (s) {
+  console.log(s); //Hello
+}).catch(function (err) {
+  console.error("fialed:" + err);
 });
+
+var p1 = Promise.reject('do something');
+
+p1.then(function (s) {
+  console.log(s); //Hello
+}).catch(function (err) {
+  console.error("fialed:" + err);
+});
+
+function list() {
+  console.log('arguments:' + JSON.stringify(arguments));
+  return Array.prototype.slice.call(arguments);
+}
+
+console.log(list(1, 2, 3));
